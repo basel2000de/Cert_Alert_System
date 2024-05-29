81674bf6238c152930635edb3193856cc9369a3b
John Sorial <john.sorial@pickware.de>
2024-05-29 23:51:41 +0200

    Use vcpkg instead of manual dependency management.

--- rss_fetcher.cpp
+++ rss_fetcher.cpp
@@ -1,32 +1,11 @@
-#include <windows.h>
-#include <wininet.h>
 #include <string>
-#include <sstream>
 #include "rss_fetcher.h"
-
-#pragma comment(lib, "wininet.lib")
+#include <cpr/cpr.h>
 
 std::string fetchRSSFeed(const std::string& url) {
-    HINTERNET hInternet = InternetOpenA("RSSFeedFetcher", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
-    if (!hInternet) {
-        return "";
-    }
 
-    HINTERNET hConnect = InternetOpenUrlA(hInternet, url.c_str(), NULL, 0, INTERNET_FLAG_RELOAD, 0);
-    if (!hConnect) {
-        InternetCloseHandle(hInternet);
-        return "";
-    }
+    cpr::Response r = cpr::Get(cpr::Url{url});
 
-    std::stringstream ss;
-    char buffer[4096];
-    DWORD bytesRead;
-    while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead) {
-        buffer[bytesRead] = '\0';
-        ss << buffer;
-    }
 
-    InternetCloseHandle(hConnect);
-    InternetCloseHandle(hInternet);
-    return ss.str();
+    return r.text;
 }
